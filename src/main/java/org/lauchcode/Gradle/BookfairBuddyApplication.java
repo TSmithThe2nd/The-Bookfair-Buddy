854b80dce04b68412ca0599eeef55b4a47085073
smity51 <smity51@gmail.com>
2022-03-07 16:54:46 -0500

    commits initial files

---
+++ src/main/java/org/lauchcode/Gradle/BookfairBuddyApplication.java
@@ -0,0 +1,13 @@
+package org.lauchcode.Gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookfairBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookfairBuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/lauchcode/Gradle/BookfairBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lauchcode.Gradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookfairBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
